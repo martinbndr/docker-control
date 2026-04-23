303c8a0c238d0ed98df7ccc26ae320e70801aa64
Martin B. <55140357+martinbndr@users.noreply.github.com>
2026-04-23 11:55:23 +0200

    Test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
+import docker
 
 load_dotenv()
 
@@ -11,9 +12,16 @@ app = Flask(__name__)
 def index():
     return "Test"
 
+@app.route("/list", methods=["GET"])
+def list():
+    client = docker.from_env()
+    containers = client.containers.list()
+    return [c.name for c in containers]
+    
+
 if __name__ == "__main__":
     debug_mode = True if os.getenv("DEBUG", "true").lower() == "true" else False
     app.run(
-        host=os.getenv("HOST", "localhost"),
+        host=os.getenv("HOST", "0.0.0.0"),
         port=os.getenv("PORT", "5000")
         )
